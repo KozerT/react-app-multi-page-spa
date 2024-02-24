23075a13152592f1c3111117f04463fb7b772ec7
KozerT <kozer.vizastaff@gmail.com>
2024-02-24 21:50:49 +0100

    feat: define products route

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,14 @@
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import Home from "./pages/Home";
+import Product from "./pages/Product";
 
-const router = createBrowserRouter([{ path: "/", element: <Home /> }, {}]);
+const router = createBrowserRouter([
+  { path: "/", element: <Home /> },
+  {
+    path: "/products",
+    element: <Product />,
+  },
+]);
 
 function App() {
   return <RouterProvider router={router}></RouterProvider>;
---
+++ src/pages/Product.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Pages = () => {
+  return <h2>The Products Page</h2>;
+};
+
+export default Pages;
