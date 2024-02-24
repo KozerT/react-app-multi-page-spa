989c4a7e5ee67661d6bae9e0cfc9fff56109db91
KozerT <kozer.vizastaff@gmail.com>
2024-02-24 21:57:46 +0100

    feat: add a Link provided by react-router dom to add a navigation from the Home to the Product page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,14 @@
-import React from "react";
+import { Link } from "react-router-dom";
 
 const Home = () => {
-  return <h1>Home Page component</h1>;
+  return (
+    <>
+      <h1>Home Page component</h1>
+      <p>
+        go to <Link to="/products">the list of products</Link>
+      </p>
+    </>
+  );
 };
 
 export default Home;
