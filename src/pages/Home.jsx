bc0970496133388d842e7fd1495347a282f74bc8
KozerT <kozer.vizastaff@gmail.com>
2024-02-25 23:39:14 +0100

    feat add a useNavigate in a Home component to navigate to the products page programatically

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,22 @@
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
+
+
+
+
 
 const Home = () => {
+
+  const navigate = useNavigate();
+
+  const navigateHandler = () => {
+    navigate('/products');
+  }
   return (
     <>
       <h1>Home Page component</h1>
+      <p>
+        <button onClick={navigateHandler}></button>
+      </p>
     </>
   );
 };
--- src/pages/Root.jsx
+++ src/pages/Root.jsx
@@ -1,11 +1,11 @@
 import { Outlet } from "react-router-dom";
 import MainNavigation from "../components/MainNavigation.jsx";
-import classes from "./Root.module.css";
+
 const Root = () => {
   return (
     <>
       <MainNavigation />
-      <main className={classes.content}>
+      <main>
         <Outlet />
       </main>
     </>
