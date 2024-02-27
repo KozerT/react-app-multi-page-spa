67a319333184bcee27922dc2fce177d2f33f4a7e
KozerT <kozer.vizastaff@gmail.com>
2024-02-27 17:38:19 +0100

    feat: create a test datain the event page, connect link to the list item, so it's redirects to the respective event detail page

--- frontend/src/pages/EventsPage.jsx
+++ frontend/src/pages/EventsPage.jsx
@@ -1,7 +1,27 @@
+import { Link } from "react-router-dom";
+
+const DUMMY_EVENTS = [
+  {
+    id: "e1",
+    title: "Some title e1",
+  },
+  {
+    id: "e2",
+    title: "Some title e2",
+  },
+];
+
 const EventsPage = () => {
   return (
     <div>
       <h1>Events page</h1>
+      <ul>
+        {DUMMY_EVENTS.map((event) => (
+          <li key={event.id}>
+            <Link to={event.id}> {event.title}</Link>
+          </li>
+        ))}
+      </ul>
     </div>
   );
 };
