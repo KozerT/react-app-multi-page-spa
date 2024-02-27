33b66630e74ae2556e3bfdb7924de4d049051857
KozerT <kozer.vizastaff@gmail.com>
2024-02-27 16:53:58 +0100

    refactor: define Root Layout component function, convert all paths to the ralative paths, convert first rout into index route

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,17 +1,24 @@
 import { RouterProvider, createBrowserRouter } from "react-router-dom";
-import HomePage from "./pages/HomePage";
-import EventsPage from "./pages/EventsPage";
-import EventDetailPage from "./pages/EventDetailPage";
-import NewEventPage from "./pages/NewEventPage";
-import EditEventPage from "./pages/EditEventPage";
+import HomePage from "./pages/HomePage.jsx";
+import EventsPage from "./pages/EventsPage.jsx";
+import EventDetailPage from "./pages/EventDetailPage.jsx";
+import NewEventPage from "./pages/NewEventPage.jsx";
+import EditEventPage from "./pages/EditEventPage.jsx";
+import RootLayout from "./pages/Root.jsx";
 
 const router = createBrowserRouter([
-  { path: "/", element: <HomePage /> },
-  { path: "/events", element: <EventsPage /> },
-  { path: "/events/:eventId", element: <EventDetailPage /> },
-  { path: "/events/new", element: <NewEventPage /> },
-  { path: "/events/new", element: <NewEventPage /> },
-  { path: "/events/:eventId/edit", element: <EditEventPage /> },
+  {
+    path: "/",
+    element: <RootLayout />,
+    children: [
+      { index: true, element: <HomePage /> },
+      { path: "events", element: <EventsPage /> },
+      { path: "events/:eventId", element: <EventDetailPage /> },
+      { path: "events/new", element: <NewEventPage /> },
+      { path: "events/new", element: <NewEventPage /> },
+      { path: "events/:eventId/edit", element: <EditEventPage /> },
+    ],
+  },
 ]);
 
 const App = () => {
