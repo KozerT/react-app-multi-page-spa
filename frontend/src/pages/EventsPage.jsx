30bf59e194f481db8de3a0900f2c5d1c6acb6dc9
KozerT <kozer.vizastaff@gmail.com>
2024-02-28 14:14:08 +0100

    refactor: implement the json() utility function

--- frontend/src/pages/EventsPage.jsx
+++ frontend/src/pages/EventsPage.jsx
@@ -1,13 +1,14 @@
-import { useLoaderData } from "react-router";
+import { json, useLoaderData } from "react-router";
 import EventsList from "../components/EventsList.jsx";
 
 export const loader = async () => {
   const response = await fetch("http://localhost:8000/events");
 
   if (!response.ok) {
-    throw new Response(
-      JSON.stringify({ message: "Could not fetch events" }, { status: 500 })
-    );
+    // throw new Response(
+    //   JSON.stringify({ message: "Could not fetch events" }, { status: 500 })
+    // );
+    return json({ message: "Could not fetch events" }, { status: 500 });
   } else {
     return response;
   }
