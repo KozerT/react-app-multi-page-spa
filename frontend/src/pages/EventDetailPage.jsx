ff8076bbcbc6ba6c2a5fe55b8c2f0144e23ce812
KozerT <kozer.vizastaff@gmail.com>
2024-03-01 23:44:53 +0100

    feat: Implement functionality to control deferred data on EventDetailPage

--- frontend/src/pages/EventDetailPage.jsx
+++ frontend/src/pages/EventDetailPage.jsx
@@ -1,21 +1,7 @@
-import { json, redirect, useRouteLoaderData } from "react-router";
+import { Await, defer, json, redirect, useRouteLoaderData } from "react-router";
 import EventItem from "../components/EventItem";
-
-export const loader = async ({ request, params }) => {
-  const id = params.eventId;
-  const response = await fetch(`http://localhost:8000/events/${id}`);
-
-  if (!response.ok) {
-    throw json(
-      { message: "Could not fetch the details for the selected events" },
-      {
-        status: 500,
-      }
-    );
-  } else {
-    return response;
-  }
-};
+import EventsList from "../components/EventsList";
+import { Suspense } from "react";
 
 export const action = async ({ params, request }) => {
   const id = params.eventId;
@@ -34,13 +20,65 @@ export const action = async ({ params, request }) => {
 };
 
 const EventDetailPage = () => {
-  const data = useRouteLoaderData("event-detail");
+  const { event, events } = useRouteLoaderData("event-detail");
 
   if (data.isError) {
     return <p>{data.message}</p>;
   }
 
-  return <EventItem event={data.event} />;
+  return (
+    <>
+      <Suspense fallback={<p style={{ textAlign: "center" }}>Loading...</p>}>
+        <Await resolve={event}>
+          {(loadedEvent) => <EventItem event={loadedEvent} />}
+        </Await>
+      </Suspense>
+      <Suspense fallback={<p style={{ textAlign: "center" }}>Loading...</p>}>
+        <Await resolve={events}>
+          {(loadedEvents) => <EventsList events={loadedEvents} />}
+        </Await>
+      </Suspense>
+    </>
+  );
 };
 
 export default EventDetailPage;
+
+export const loader = async ({ request, params }) => {
+  const id = params.eventId;
+
+  return defer({
+    event: loadEvent(id),
+    events: loadEvents(),
+  });
+};
+
+const loadEvent = async () => {
+  const response = await fetch(`http://localhost:8000/events/${id}`);
+
+  if (!response.ok) {
+    throw json(
+      { message: "Could not fetch the details for the selected events" },
+      {
+        status: 500,
+      }
+    );
+  } else {
+    const resData = await response.json();
+    return resData.events;
+  }
+};
+
+const loadEvents = async () => {
+  const response = await fetch("http://localhost:8000/events");
+
+  if (!response.ok) {
+    // throw new Response(
+    //   JSON.stringify({ message: "Could not fetch events" }, { status: 500 })
+    // );
+    return json({ message: "Could not fetch events" }, { status: 500 });
+  } else {
+    const resData = await response.json();
+    return resData.events;
+  }
+};
