77d51476db9f3826b21ad767e58a29f8fae7f32e
KozerT <kozer.vizastaff@gmail.com>
2024-03-04 23:05:51 +0100

    feat: implement token cleaning functionality after one hour timeframe

--- frontend/src/pages/Root.jsx
+++ frontend/src/pages/Root.jsx
@@ -1,13 +1,25 @@
+import { useEffect } from "react";
 import MainNavigation from "../components/MainNavigation.jsx";
-import { Outlet, useNavigation } from "react-router";
+import { Outlet, useLoaderData } from "react-router";
+import { useSubmit } from "react-router-dom";
 
 const RootLayout = () => {
-  // const navigation = useNavigation();
+  const token = useLoaderData();
+  const submit = useSubmit;
+  useEffect(() => {
+    if (!token) {
+      return;
+    }
+
+    setTimeout(() => {
+      submit(null, { action: "/logout", method: "post" });
+    }, 1 * 60 * 60 * 1000);
+  }, [token, submit]);
+
   return (
     <>
       <MainNavigation />
       <main>
-        {/* {navigation.state === "loading" && <p>Loading...</p>} */}
         <Outlet />
       </main>
     </>
--- frontend/src/util/auth.js
+++ frontend/src/util/auth.js
@@ -1,20 +1,38 @@
-import { redirect } from "react-router";
+import { redirect } from "react-router-dom";
 
-export const getAuthToken = () => {
+export function getTokenDuration() {
+  const storedExpirationDate = localStorage.getItem("expiration");
+  const expirationDate = new Date(storedExpirationDate);
+  const now = new Date();
+  const duration = expirationDate.getTime() - now.getTime();
+  return duration;
+}
+
+export function getAuthToken() {
   const token = localStorage.getItem("token");
+
+  if (!token) {
+    return null;
+  }
+
+  const tokenDuration = getTokenDuration();
+
+  if (tokenDuration < 0) {
+    return "EXPIRED";
+  }
+
   return token;
-};
+}
 
-export const tokenLoader = () => {
-  return getAuthToken();
-};
+export function tokenLoader() {
+  const token = getAuthToken();
+  return token;
+}
 
-export const checkAuthLoader = () => {
+export function checkAuthLoader() {
   const token = getAuthToken();
 
   if (!token) {
     return redirect("/auth");
   }
-
-  return null;
-};
+}
